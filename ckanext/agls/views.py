f74fa8ffda84719b4a5a6724652b71a5f865a48d
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2020-01-27 14:47:29 +0200

    Enable gmd route

--- ckanext/agls/views.py
+++ ckanext/agls/views.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from flask import Blueprint, jsonify
+
+from flask import Blueprint, jsonify, make_response
+
+import ckan.lib.base as base
+import ckan.model as model
 import ckan.plugins.toolkit as tk
+import ckan.views.dataset as dataset
+
 
 agls = Blueprint("agls", __name__)
 
@@ -11,8 +17,43 @@ def get_blueprints():
     return [agls]
 
 
-def gmd(id):
-    raise NotImplemented("GMD endpoint not implemented for python 3")
+def gmd(id, package_type="dataset"):
+    format = "html"
+
+    context = {
+        "model": model,
+        "session": model.Session,
+        "user": tk.c.user or tk.c.author,
+        "for_view": True,
+        "auth_user_obj": tk.c.userobj,
+    }
+    data_dict = {"id": id}
+    # check if package exists
+    try:
+        pkg_dict = tk.get_action("package_show")(context, data_dict)
+        pkg = context["package"]
+    except tk.NotFound:
+        return tk.abort(404, tk._("Dataset not found"))
+    except tk.NotAuthorized:
+        return tk.abort(401, tk._("Unauthorized to read package %s") % id)
+
+    dataset._setup_template_variables(context, {"id": id}, package_type)
+    template = "package/read.gmd"
+    extra_vars = {"dataset_type": package_type, "pkg": pkg, "pkg_dict": pkg_dict}
+
+    try:
+        response = make_response(base.render(template, extra_vars))
+    except base.TemplateNotFound:
+        msg = tk._(
+            "Viewing {package_type} datasets in {format} format is "
+            "not supported (template file {file} not found).".format(
+                package_type=package_type, format=format, file=template
+            )
+        )
+        return tk.abort(403, msg)
+    response.headers["Content-Type"] = "application/vnd.iso.19139+xml; charset=utf-8"
+    response.headers["Content-Disposition"] = "attachment; filename=" + id + ".xml"
+    return response
 
 
 def geo_autocomplete():
@@ -62,9 +103,7 @@ def geo_latlon():
     return {}
 
 
-agls.add_url_rule(
-    "/dataset/<id>/gmd", view_func=gmd,
-)
+agls.add_url_rule("/<package_type>/<id>/gmd", view_func=gmd)
 agls.add_url_rule(
     "/api/2/util/gazetteer/autocomplete", view_func=geo_autocomplete,
 )
